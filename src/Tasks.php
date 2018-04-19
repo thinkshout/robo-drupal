3e1c25d1387095e6b9e1fef460dc818c441aee8c
Gabe Carleton-Barnes <gabe@thinkshout.com>
2018-04-19 14:18:38 -0400

    Seperate chmod commands.

--- src/Tasks.php
+++ src/Tasks.php
@@ -467,7 +467,7 @@ class Tasks extends \Robo\Tasks
     }
     $default_dir = 'code/' . $web_root . 'sites/default';
     $sftp_command .= ' << EOF
-chmod 644 ' . $default_dir . '
+chmod 755 ' . $default_dir . ';
 chmod 644 ' . $default_dir . '/settings.php
 EOF';
     $this->_exec($sftp_command);
