624b324ff56ce5a1c8579037886f761e71c2fa78
Gabe Carleton-Barnes <gabe@thinkshout.com>
2018-04-19 15:44:06 -0400

    Remove EOFs from sftp command, try robo exec this way.

--- src/Tasks.php
+++ src/Tasks.php
@@ -466,11 +466,12 @@ class Tasks extends \Robo\Tasks
       $web_root = '';
     }
     $default_dir = 'code/' . $web_root . 'sites/default';
-    $sftp_command .= ' << EOF
+    $sftp_command .= ' <<
 chmod 755 ' . $default_dir . '
 chmod 755 ' . $default_dir . '/settings.php
-EOF';
-    exec($sftp_command);
+';
+//    exec($sftp_command);
+    $this->_exec($sftp_command);
 
     // Run the installation.
     $result = $this->taskExec($install_cmd)
