0755e068d2adbc1d82e050037d04aee5dc9ef2ee
Maria Fisher <mariacha@gmail.com>
2018-09-09 12:34:30 -0700

    Typo.

--- src/Tasks.php
+++ src/Tasks.php
@@ -867,7 +867,7 @@ chmod 755 ' . $default_dir . '/settings.php';
     $project_properties = $this->getProjectProperties();
     if (!$this->databaseSourceOfTruth()) {
       $this->say('No source database configured.');
-      $this->say('To use this command, you must a string from the databaseSourceOfTruth() method in your project RoboFile.php.');
+      $this->say('To use this command, you must return a string from the databaseSourceOfTruth() method in your project RoboFile.php.');
       return FALSE;
     }
 
