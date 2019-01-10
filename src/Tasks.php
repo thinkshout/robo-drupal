777cd76f71edacc984ab01ec487a10887c2b1a54
Gabe Carleton-Barnes <gabe@thinkshout.com>
2019-01-09 19:10:19 -0800

    Revert "Issue #18 attempting to add drush aliases during configure."
    This reverts commit 7d51e9edbed579bdeb1815d8f5902668f22af929.

--- src/Tasks.php
+++ src/Tasks.php
@@ -189,13 +189,7 @@ class Tasks extends \Robo\Tasks
       ->line('TS_INSTALL_PROFILE=' . $this->projectProperties['install_profile'])
       ->run();
 
-    // Write drush aliases.
-    $this->say('Writing drush aliases to .yml file.');
-    // Run the installation.
-    $result2 = $this->taskExec('drush site:alias-convert')
-      ->run();
-
-    return $result && $result2;
+    return $result;
   }
 
   /**
