12d1a55c5091cab202a7b89cebfc6005074362b7
rjacobsen0 <rjacobsen0@gmail.com>
2019-05-17 17:20:04 -0700

    trying another method of getting the current git branch

--- src/Tasks.php
+++ src/Tasks.php
@@ -297,7 +297,12 @@ class Tasks extends \Robo\Tasks
    * @return \Robo\Result
    */
   function applyUpdate() {
-    $currentBranch = $this->taskExec('git branch | grep \* | cut -d " " -f2');
+
+    $stringfromfile = file('.git/HEAD', FILE_USE_INCLUDE_PATH);
+    $firstLine = $stringfromfile[0]; //get the string from the array
+    $explodedstring = explode("/", $firstLine, 3); //seperate out by the "/" in the string
+    $currentBranch = $explodedstring[2]; //get the one that is always the branch name
+
     $out = $this->taskExec('echo Current git branch is: $currentBranch')
       ->run();
 
