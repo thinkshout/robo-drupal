122ee17f54ab55533cc30359503da97fff86730d
Maria Fisher <maria.fisher@thinkshout.com>
2019-05-29 18:07:52 -0700

    Revert "Making a doc change."
    This reverts commit 4d0bddc95a8a19b601f0849e81476d16526c7d38.

--- src/Tasks.php
+++ src/Tasks.php
@@ -44,7 +44,7 @@ class Tasks extends \Robo\Tasks
   }
 
   /**
-   * Initialize the project for the first time MARIA STUFF.
+   * Initialize the project for the first time.
    *
    * @return \Robo\Result
    */
